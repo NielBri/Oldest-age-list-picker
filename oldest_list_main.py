5264f4d6442e46a69610b3628200f1e0a4aaf854
NielBri <briones.nielmatthew@gmail.com>
2024-10-22 10:59:54 +0800

    This is the first commit. Pseudocode.

---
+++ oldest_list_main.py
@@ -0,0 +1,8 @@
+# Make a list
+# Name input
+# Age input
+# Array to put the data in
+# Add another entry
+# Assume that the first entry is the oldest
+# Add a function that compares a new entry to the oldest entry
+# Print the oldest person in the list after it checks to see who is the oldest in the list
